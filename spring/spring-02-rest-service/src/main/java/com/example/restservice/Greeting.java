0f355fb3e8e59e15cac887c6c218d8226ffe2abb
wendel.araujo <wendelca@gmail.com>
2021-11-10 20:35:36 -0300

    spring-02-rest

---
+++ spring/spring-02-rest-service/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.restservice;
+
+public class Greeting {
+    public final long id;
+    public final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
+
